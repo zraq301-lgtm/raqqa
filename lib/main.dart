82599fa90ee9eb7eacfd0b8f2c55f0f6ac4eb0c7
zraq301-lgtm <zraq301@gmail.com>
2026-01-28 01:39:46 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  // التأكد من تهيئة جميع الخدمات قبل تشغيل التطبيق
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const RaqqaApp());
-}
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RaqqaSafeWrapper(),
+    ));
 
-class RaqqaApp extends StatelessWidget {
-  const RaqqaApp({super.key});
-
+class RaqqaSafeWrapper extends StatefulWidget {
+  const RaqqaSafeWrapper({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Raqqa App',
-      debugShowCheckedModeBanner: false, // إخفاء علامة الـ Debug
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyWebView(),
-    );
-  }
+  State<RaqqaSafeWrapper> createState() => _RaqqaSafeWrapperState();
 }
 
-class MyWebView extends StatefulWidget {
-  const MyWebView({super.key});
-
-  @override
-  State<MyWebView> createState() => _MyWebViewState();
-}
-
-class _MyWebViewState extends State<MyWebView> {
-  late final WebViewController controller;
-  bool isLoading = true; // متغير لتتبع حالة التحميل
+class _RaqqaSafeWrapperState extends State<RaqqaSafeWrapper> {
+  late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
-    
-    // إعداد الـ Controller بشكل احترافي
-    controller = WebViewController()
+    _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
         NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() { isLoading = true; }); // إظهار مؤشر التحميل
-          },
-          onPageFinished: (String url) {
-            setState(() { isLoading = false; }); // إخفاء مؤشر التحميل عند الانتهاء
-          },
+          onPageFinished: (url) => debugPrint("Loaded: $url"),
         ),
       )
       ..loadRequest(Uri.parse('https://raqqa-v6cd.vercel.app/'));
@@ -58,29 +30,16 @@ class _MyWebViewState extends State<MyWebView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // SafeArea تضمن عدم تداخل التطبيق مع ساعة الموبايل أو الكاميرا الأمامية
-      body: SafeArea(
-        child: Stack(
-          children: [
-            WebViewWidget(controller: controller),
-            // إظهار دائرة تحميل فوق الموقع حتى يفتح بالكامل
-            if (isLoading)
-              const Center(
-                child: CircularProgressIndicator(color: Colors.blue),
-              ),
-          ],
-        ),
-      ),
-      
-      // زر الفيديوهات بتصميم عصري
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => controller.loadRequest(
-          Uri.parse('https://raqqa-v6cd-haq5y46jg-raqqs-projects.vercel.app/vid.html'),
-        ),
-        label: const Text('الفيديوهات'),
-        icon: const Icon(Icons.video_library),
-        backgroundColor: Colors.blueAccent,
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _controller.canGoBack()) {
+          _controller.goBack(); // الرجوع للخلف داخل الموقع
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        body: SafeArea(child: WebViewWidget(controller: _controller)),
       ),
     );
   }
