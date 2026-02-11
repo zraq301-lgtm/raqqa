d527490893994ae19b54902199ec8a878a5bd65c
zraq301-lgtm <zraq301@gmail.com>
2026-02-11 05:46:59 +0200

    Delete src/page directory

--- src/page/Feelings.jsx
+++
@@ -1,8 +0,0 @@
-export default function Feelings() {
-  return (
-    <div className="page-container">
-      <h1>عالم الأحاسيس</h1>
-      <p>مساحة خاصة للتعبير عن مشاعرك وتتبع حالتك النفسية.</p>
-    </div>
-  );
-}
--- src/page/Intimacy.jsx
+++
@@ -1,8 +0,0 @@
-export default function Intimacy() {
-  return (
-    <div className="page-container">
-      <h1>المودة والخصوصية</h1>
-      <p>ركن خاص لتعزيز الروابط العاطفية بخصوصية تامة.</p>
-    </div>
-  );
-}
