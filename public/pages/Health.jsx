cc423931fefeffcf34a29634fba23a8370f6c432
zraq301-lgtm <zraq301@gmail.com>
2026-02-11 04:51:40 +0200

    Delete public directory

--- public/pages/Health.jsx
+++
@@ -1,8 +0,0 @@
-export default function Health() {
-  return (
-    <div className="page-container health-page">
-      <h1>قسم صحتك</h1>
-      <p>تحليل صحي شامل بالذكاء الاصطناعي لمتابعة حالتك اليومية.</p>
-    </div>
-  );
-}
