8e753cf1b98c5b3e0d7c0c679619972aad273f37
zraq301-lgtm <zraq301@gmail.com>
2026-02-25 01:42:46 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppSwitcher from './AppSwitcher'; // استدعاء المحول بدلاً من App
+import { BrowserRouter } from 'react-router-dom'; // إضافة التوجيه هنا
+import AppSwitcher from './AppSwitcher'; 
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppSwitcher />
+    <BrowserRouter> {/* تغليف التطبيق بالكامل بالموجّه */}
+      <AppSwitcher />
+    </BrowserRouter>
   </React.StrictMode>
 );
