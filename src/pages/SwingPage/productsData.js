211f6b585209c4d78c26e788c0b03305d4e9e702
zraq301-lgtm <zraq301@gmail.com>
2026-04-07 05:32:14 +0200

    Create productsData.js

---
+++ src/pages/SwingPage/productsData.js
@@ -0,0 +1,84 @@
+// productsData.js
+export const allProducts = [
+  {
+    id: "1005008972429187",
+    name: "NEW Customized Print T Shirt DIY Photo Logo",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/S32e1b21c409e41029d3bb8d7d12bfd73D.jpg",
+    price: "4.99",
+    oldPrice: "6.24",
+    discount: "20%"
+  },
+  {
+    id: "1005009699946503",
+    name: "Classic Iconic Route 66 Emblem Motorcycle T-Shirt",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/Sd758e47741ca452a99fe5aa4e7dfe4e8I.jpg",
+    price: "39.22",
+    oldPrice: "74.04",
+    discount: "47%"
+  },
+  {
+    id: "1005009727866548",
+    name: "Hoodie Y2K Retro zip up Camouflage Pattern",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/Sb71ff81ac38c43128cc2e0d031164eda6.jpg",
+    price: "0.44",
+    oldPrice: "0.55",
+    discount: "20%"
+  },
+  {
+    id: "1005007495435769",
+    name: "Mens Clothing Classic Japan Short Sleeve Ice Silk",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/S00ca129c0aaa46c596afb8200c51c9c1s.jpeg",
+    price: "3.01",
+    oldPrice: "6.57",
+    discount: "54%"
+  },
+  {
+    id: "1005008029759451",
+    name: "Summer Animals Vintage leopard T-shirt",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/Sa6ca4287e67243e39142b69cce4c37d7K.jpg",
+    price: "6.17",
+    oldPrice: "29.34",
+    discount: "79%"
+  },
+  {
+    id: "1005003097419931",
+    name: "Cool Carp Fishing Men's T-shirt 3D Print",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/Hcbdb488398dc484da9a7b2385b770420m.jpg",
+    price: "6.17",
+    oldPrice: "29.34",
+    discount: "79%"
+  },
+  {
+    id: "1005008916604902",
+    name: "Hot selling 3D printed chess T-shirt",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/S6eafbc9d78ee431cb5cf39f434e34716G.jpg",
+    price: "12.05",
+    oldPrice: "42.93",
+    discount: "72%"
+  },
+  {
+    id: "1005011773399269",
+    name: "Brazil Jesus Print Soccer Jersey 2026",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/S8ebe613dbb504f7eb522ff7290c313d55.jpeg",
+    price: "33.49",
+    oldPrice: "34.34",
+    discount: "2%"
+  },
+  {
+    id: "1005009864885248",
+    name: "Instant Nursing Assistant - Just Add Coffee",
+    url: "https://rzekl.com/g/1e8d1144944131b5064a16525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ",
+    image: "https://ae-pic-a1.aliexpress-media.com/kf/S0e6061c0460d437191d5b2353becec25y.jpg",
+    price: "3.78",
+    oldPrice: "7.53",
+    discount: "50%"
+  }
+];
